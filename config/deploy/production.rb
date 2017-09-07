256d439048cc7902a3fa775b373f0ac9f4d6b538
Esteban Mino <efmino@uc.cl>
2017-09-07 11:33:36 -0300

    deploy ip

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,6 @@
+
+server '54.174.131.159', user: 'deploy', roles: %w{web app db}
+
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
